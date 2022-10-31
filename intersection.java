70ada8127e43cbe26f3d76b7772229a8d0917b71
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-10-31 21:32:12 +0530

    Create intersection.java

---
+++ intersection.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Set < Integer > s1 = new HashSet < Integer > ();
+    Set < Integer > s2 = new HashSet < Integer > ();
+    s1.add (10);
+    s1.add (30);
+    s1.add (20);
+    s1.add (40);
+    s2.add (10);
+    s2.add (30);
+    s2.add (32);
+    s2.add (50);
+    s2.add (70);
+
+    intersection (s1, s2);
+   
+ }
+
+  static void intersection (Set < Integer > s1, Set < Integer > s2)
+  {
+    // intersection
+    s1.retainAll (s2);
+    System.out.println (s1);
+  }
+}
