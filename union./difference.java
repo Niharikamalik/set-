94617bf8349c2a629847b2d26e0cd52d09751791
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-10-31 21:36:55 +0530

    Create difference.java

---
+++ union./difference.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main{
+    public static void main (String[] args) {
+        Set<Integer> s1 = new HashSet<Integer>();
+        Set<Integer> s2 = new HashSet<Integer>();
+        s1.add(10);
+        s1.add(30);
+        s1.add(20);
+        s1.add(40);
+        s2.add(10);
+        s2.add(30);
+        s2.add(32);
+        s2.add(50);
+        s2.add(70);
+       
+        union(s1,s2);
+        
+    }
+    static void union(Set<Integer> s1 ,Set<Integer> s2){
+        // union of sets 
+        s1.addAll(s2);
+        System.out.println(s1);
+    }
+}   
